e8cf8e336bcab3e6ebc04b7849726a37c87a8020
Alberto6789 <121826084+Alberto6789@users.noreply.github.com>
2023-08-20 13:07:48 -0600

    Update botclient.py

--- botclient.py
+++ botclient.py
@@ -1,6 +1,6 @@
-﻿from telethon import TelegramClient as Client
-from telethon import events
-from telethon import Button
+﻿from pyrogram import TelegramClient as Client
+from pyrogram import events
+from pyrogram import Button
 
 import aiohttp
 import aiofiles
